b3df6f0449a3d889b38e11a0a15edad410827ebc
Aryaman Mishra <115460120+AryamanAMD@users.noreply.github.com>
2023-02-22 01:12:08 +0530

    added strategy feature

--- bot.py
+++ bot.py
@@ -180,6 +180,69 @@ async def _command(ctx,arg1,arg2,arg3,arg4,arg5):
   
   ff1.Cache.clear_cache('cache')   
 
+
+@client.command(name="strategy")
+async def _command(ctx,arg1,arg2):
+  year=arg1
+  track=arg2
+  await ctx.send("{},{}".format(year,track))
+  race = ff1.get_session(year, track, 'R')
+  laps = race.load_laps(with_telemetry=True)
+  driver_stints = laps[['Driver', 'Stint', 'Compound', 'LapNumber']].groupby(
+    ['Driver', 'Stint', 'Compound']
+).count().reset_index()
+  driver_stints = driver_stints.rename(columns={'LapNumber': 'StintLength'})
+  driver_stints = driver_stints.sort_values(by=['Stint'])
+  compound_colors = {
+    'SOFT': '#FF3333',
+    'MEDIUM': '#FFF200',
+    'HARD': '#EBEBEB',
+    'INTERMEDIATE': '#39B54A',
+    'WET': '#00AEEF',
+}
+  plt.rcParams["figure.figsize"] = [15, 10]
+  plt.rcParams["figure.autolayout"] = True
+
+  fig, ax = plt.subplots()
+
+  for driver in race.results['Abbreviation']:
+      stints = driver_stints.loc[driver_stints['Driver'] == driver]
+      
+      previous_stint_end = 0
+      for _, stint in stints.iterrows():
+          plt.barh(
+              [driver], 
+              stint['StintLength'], 
+              left=previous_stint_end, 
+              color=compound_colors[stint['Compound']], 
+              edgecolor = "black"
+          )
+          
+          previous_stint_end = previous_stint_end + stint['StintLength']
+          
+  # Set title
+  plt.title(f'Race strategy - {track} {year}')
+          
+  # Set x-label
+  plt.xlabel('Lap')
+
+  # Invert y-axis 
+  plt.gca().invert_yaxis()
+
+  # Remove frame from plot
+  ax.spines['top'].set_visible(False)
+  ax.spines['right'].set_visible(False)
+  ax.spines['left'].set_visible(False)
+
+  plt.savefig('strategy.png', dpi=300)
+
+  #plt.show()
+  with open('lapcomparison.png', 'rb') as f:
+    picture = discord.File(f)
+    await ctx.send(file=picture)
+  ff1.Cache.clear_cache('cache')   
+
+
  
 #keep_alive()
 my_secret = os.environ['TOKEN']
